f9714f546fd02a59ece5a833b28276c8c3508283
@esales.luis.silva <luis.silva@esales.com.br>
2023-10-18 17:08:49 -0300

    Correção do escopo dos métodos da classe Usuário

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,69 @@ public class Main {
     public static List<Pedido> listaPedidos = new ArrayList<>();
     public static Usuario usuarioAtual = null;
 
+    public static void contPedidos(ArrayList <Pedido> pedidos) {
+        int qntTotal=0, qntAprovados=0, qntReprovados=0;
+        for (Pedido p : pedidos) {
+            qntTotal++;
+            if (p.getStatusAprovacao()) {
+                qntAprovados++;
+            }
+            else {
+                qntReprovados++;
+            }
+        }
+
+        System.out.printf("Quantidade de pedidos : [%d]\n", qntTotal);
+        System.out.printf("Quantidade de pedidos aprovados : [%d] | [%.2f%%]\n", qntAprovados, ((double) qntAprovados/qntTotal)*100);
+        System.out.printf("Quantidade de pedidos reprovados : [%d] | [%.2f%%]", qntReprovados, ((double) qntAprovados/qntReprovados)*100);
+    }
+
+    public static double calculaMedia30(ArrayList <Pedido> pedidos, String dataAtual) {
+        double valorTotal = 0, media = 0;
+        int quantidade = 0;
+
+        for (Pedido p : pedidos) {
+            if(verificaData(p.getDataPedido())) {
+                quantidade++;
+                valorTotal += p.getValorTotal();
+            }
+        }
+
+        if (quantidade > 0) {
+            media = valorTotal / quantidade;
+        }
+
+        return media;
+    }
+
+    public static void totalDepartamento30(ArrayList<Pedido> pedidos, ArrayList<Departamento> departamentos) {
+        for (Departamento d : departamentos) {
+            double valorTotalDepartamento = 0;
+            for (Pedido p : pedidos) {
+                if (verificaData(p.getDataPedido()) && p.getDepartamento().getIdentificador() == d.getIdentificador()) {
+                    valorTotalDepartamento += p.getValorTotal();
+                }
+            }
+            System.out.printf("Valor total do departamento %d nos últimos 30 dias: %.2f\n", d.getIdentificador(), valorTotalDepartamento);
+        }
+    }
+
+    public static Pedido maiorPedidoAberto(ArrayList <Pedido> pedidos) {
+        Pedido maiorP = null;
+        double maiorV = 0;
+
+        for (Pedido p : pedidos) {
+            if (p.getStatusAprovacao()) {
+                if (maiorP == null || p.getValorTotal() > maiorV) {
+                    maiorP = p;
+                    maiorV = p.getValorTotal();
+                }
+            }
+        }
+
+        return maiorP;
+    }
+
     public static void removePedidoPorId(int id, Funcionario funcionario){
         Optional<Pedido> pedidoFiltrado = listaPedidos.stream().filter(pedido -> pedido.getId() == id).findFirst();
         if(pedidoFiltrado.isPresent()){
@@ -19,6 +82,22 @@ public class Main {
         else{
             System.out.println("Pedido não encontrado");
         }
+    }
+
+    public static boolean verificaData(String dataPedido) {
+        String[] pPedido = dataPedido.split("-");
+
+        int anoPedido = Integer.parseInt(pPedido[2]);
+        int mesPedido = Integer.parseInt(pPedido[1]);
+        int diaPedido = Integer.parseInt(pPedido[0]);
+
+        LocalDate dtDataPedido = LocalDate.of(anoPedido, mesPedido, diaPedido);
+        LocalDate dtDataAtual = LocalDate.now();
+
+        long dias = ChronoUnit.DAYS.between(dtDataPedido, dtDataAtual);
+
+
+        return dias <= 30;
     }
 
     public static void selecionaUsuario(Scanner scan){
